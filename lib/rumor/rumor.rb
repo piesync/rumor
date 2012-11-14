b9d853f209c9ca6184a7241adbd0e84baaaf9b01
Mattias Putman <mattias.putman@gmail.com>
2012-11-14 17:12:40 +0100

    changes to rumor model

--- lib/rumor/rumor.rb
+++ lib/rumor/rumor.rb
@@ -3,33 +3,75 @@ module Rumor
   # Public: A Rumor represents an event that manifests itself
   # in a program. An event frequently describes an action
   # that a user has executed.
+  #
+  # Examples
+  #
+  #   Rumor.new(:upgraded).on('user1').mention(plan: :tasty).tag(:business).spread
+  #
   class Rumor
 
     # Public: A rumor has a subject, the event name.
     # this is required for every rumor.
     attr_accessor :subject
 
-    # Public: The starter of the rumor.
+    # Public: Wo the rumor is about
     # This is mostly the user that executed the event.
-    attr_accessor :from
+    attr_accessor :about
 
     # Public: The mentions of the rumor.
     # All the information a rumor mentions about an event.
     attr_accessor :mentions
 
-    # Public: Every rumor has some categories.
-    # A Rumor can be categorized in multiple optional categories.
-    attr_accessor :categories
+    # Public: Every rumor has some tags.
+    # A Rumor can be categorized in multiple optional tags.
+    attr_accessor :tags
 
     # Public: Creates a new rumor.
     #
-    # attributes - Attributes of the rumor (Hash), subject is required
-    def initialize attributes
+    # subject - subject of the rumor, required
+    def initialize subject
+      @subject = subject
+      @tags = []
+      @mentions = {}
+    end
+
+    # Public: Tell who/what the rumor is concerning.
+    def on about
+      @about = about
+    end
+
+    # Public: Mention some things in the rumor.
+    # Merges with already mentioned information.
+    def mention mentions
+      @mentions.merge!(mentions) do |key, old_val, new_val|
+        if old.kind_of?(Array)
+          old_val + new_val
+        elsif old.kind_of?(Hash)
+          old_val.merge new_val
+        else
+          new_val
+        end
+      end
+    end
+
+    # Public: Add some tags to the rumor.
+    def tag tags
+      @tags << tags
     end
 
     # Public: Copy a rumor while altering information.
     def copy &alter
       Rumor.new(hash).tap &alter
+    end
+
+    # Spread the rumor to all applicable channels.
+    #
+    # conditions - some conditions on the spreading (Hash).
+    #             :only - The channels to spread to (and none other).
+    #             :except - The channels not to spread to.
+    #
+    # Returns nothing.
+    def spread conditions
     end
 
     # Public: Check if the given rumor is a subset of this rumor.
@@ -41,7 +83,7 @@ module Rumor
       # Check if mentions are a subset.
       _mentions = rumors.mentions.all? { |k, v| mentions[k] == v }
       # Check if categories are a subset.
-      _categories = (categories | rumor.categories) == categories
+      _tags = (tags | rumor.tags) == categories
       # All those checks must be true
       _subject && _from && _mentions && _categories
     end
