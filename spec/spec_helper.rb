9e2d72dd947df1f624e8bfe71f1961eec89ca7b7
Mattias Putman <mattias.putman@gmail.com>
2014-07-26 12:00:00 +0200

    fix specs; but need more specs for handlers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,10 @@ require 'mocha/setup'
 
 require 'rumor'
 require 'rumor/async/resque'
-
 require 'rumor/async/sucker_punch'
+
+Rumor.async_handler = Rumor::Async::Resque
+
 require 'sucker_punch/testing/inline'
 Resque.inline = true
 
